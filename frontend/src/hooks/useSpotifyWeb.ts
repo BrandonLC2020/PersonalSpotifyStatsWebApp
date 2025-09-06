131a0830892d7c78f7e67237adf74f553fe2459c
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2025-09-06 17:34:45 -0400

    I think it's working

--- frontend/src/hooks/useSpotifyWeb.ts
+++ frontend/src/hooks/useSpotifyWeb.ts
@@ -24,7 +24,7 @@ const useSpotifyWeb = () => {
 
         if (data.SecretString) {
           const secret = JSON.parse(data.SecretString);
-          const accessToken = secret.SPOTIFY_ACCESS_TOKEN; // Assumes your secret has this key
+          const accessToken = secret.spotify_refresh_token; // Assumes your secret has this key
 
           if (accessToken) {
             const sp = new SpotifyWebApi();
